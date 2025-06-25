9320aa73957a0fc87f9cc59bc4c70b7d620565e5
ZhaoZheCobo <139831276+ZhaoZheCobo@users.noreply.github.com>
2025-06-25 10:45:59 +0800

    update jinja2 engine

--- cobo-mpc-auth-data-verify/validator/statement.go
+++ cobo-mpc-auth-data-verify/validator/statement.go
@@ -1,11 +1,15 @@
 package validator
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"encoding/json"
 	"fmt"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/kluctl/kluctl/lib/go-jinja2"
+	"github.com/nikolalohinski/gonja/v2"
+	"github.com/nikolalohinski/gonja/v2/exec"
+	"github.com/nikolalohinski/gonja/v2/loaders"
 )
 
 type StatementBuilder struct {
@@ -18,25 +22,94 @@ func NewStatementBuilder(template string) *StatementBuilder {
 	}
 }
 
-// getJinja2Filters returns a list of filters to be used with Jinja2
-func getJinja2Filters() []jinja2.Jinja2Opt {
-	return []jinja2.Jinja2Opt{
-		jinja2.WithFilter("toString", `import json
-def toString(v):
-    return json.dumps(v, ensure_ascii=False)`),
-		jinja2.WithFilter("toInt", `def toInt(v):
-    return int(v)`),
-		jinja2.WithFilter("len", `def len(v):
-    return __builtins__['len'](v)`),
-		jinja2.WithFilter("toList1", `import json
-def toList1(v):
-    return json.dumps([str(x) for x in v if x], ensure_ascii=False)`),
-		jinja2.WithFilter("toList2", `import json
-def toList2(v):
-    return json.dumps([[str(x) for x in row if x] for row in v if row], ensure_ascii=False)`),
-		jinja2.WithFilter("toRules", `import json
-def toRules(v):
-    return json.dumps([{str(k): str(v) for k, v in x.items()} for x in v if x], ensure_ascii=False)`),
+// getGonjaFilters returns a list of custom filters to be used with Gonja
+func getGonjaFilters() map[string]exec.FilterFunction {
+	return map[string]exec.FilterFunction{
+		"toString": func(e *exec.Evaluator, in *exec.Value, params *exec.VarArgs) *exec.Value {
+			bytes, _ := json.Marshal(in.Interface())
+			return exec.AsValue(string(bytes))
+		},
+		"toInt": func(e *exec.Evaluator, in *exec.Value, params *exec.VarArgs) *exec.Value {
+			switch val := in.Interface().(type) {
+			case float64:
+				return exec.AsValue(int(val))
+			case int:
+				return exec.AsValue(val)
+			case string:
+				var result int
+				fmt.Sscanf(val, "%d", &result)
+				return exec.AsValue(result)
+			default:
+				return exec.AsValue(0)
+			}
+		},
+		"len": func(e *exec.Evaluator, in *exec.Value, params *exec.VarArgs) *exec.Value {
+			switch val := in.Interface().(type) {
+			case []interface{}:
+				return exec.AsValue(len(val))
+			case map[string]interface{}:
+				return exec.AsValue(len(val))
+			case string:
+				return exec.AsValue(len(val))
+			default:
+				return exec.AsValue(0)
+			}
+		},
+		"toList1": func(e *exec.Evaluator, in *exec.Value, params *exec.VarArgs) *exec.Value {
+			if slice, ok := in.Interface().([]interface{}); ok {
+				var result []string
+				for _, item := range slice {
+					if item != nil {
+						result = append(result, fmt.Sprintf("%v", item))
+					}
+				}
+				bytes, _ := json.Marshal(result)
+				return exec.AsValue(string(bytes))
+			}
+			return exec.AsValue("[]")
+		},
+		"toList2": func(e *exec.Evaluator, in *exec.Value, params *exec.VarArgs) *exec.Value {
+			if slice, ok := in.Interface().([]interface{}); ok {
+				var result [][]string
+				for _, row := range slice {
+					if rowSlice, ok := row.([]interface{}); ok {
+						var rowResult []string
+						for _, item := range rowSlice {
+							if item != nil {
+								rowResult = append(rowResult, fmt.Sprintf("%v", item))
+							}
+						}
+						if len(rowResult) > 0 {
+							result = append(result, rowResult)
+						}
+					}
+				}
+				bytes, _ := json.Marshal(result)
+				return exec.AsValue(string(bytes))
+			}
+			return exec.AsValue("[]")
+		},
+		"toRules": func(e *exec.Evaluator, in *exec.Value, params *exec.VarArgs) *exec.Value {
+			if slice, ok := in.Interface().([]interface{}); ok {
+				var result []map[string]string
+				for _, item := range slice {
+					if mapItem, ok := item.(map[string]interface{}); ok {
+						ruleMap := make(map[string]string)
+						for k, v := range mapItem {
+							if v != nil {
+								ruleMap[k] = fmt.Sprintf("%v", v)
+							}
+						}
+						if len(ruleMap) > 0 {
+							result = append(result, ruleMap)
+						}
+					}
+				}
+				bytes, _ := json.Marshal(result)
+				return exec.AsValue(string(bytes))
+			}
+			return exec.AsValue("[]")
+		},
 	}
 }
 
@@ -47,22 +120,55 @@ func (s *StatementBuilder) Build(bizData string) (string, error) {
 		return "", fmt.Errorf("error parsing JSON data: %w", err)
 	}
 
-	options := append([]jinja2.Jinja2Opt{
-		jinja2.WithGlobals(data),
-	}, getJinja2Filters()...)
-
-	j2, err := jinja2.NewJinja2("python3", 1, options...)
+	template, err := getGonjaTemplate(s.template)
 	if err != nil {
-		return "", fmt.Errorf("error initializing jinja2: %w", err)
+		return "", fmt.Errorf("error parsing template: %w", err)
 	}
-	defer j2.Close()
 
-	message, err := j2.RenderString(s.template)
+	// Create context with data
+	context := exec.NewContext(data)
+
+	// Render the template
+	message, err := template.ExecuteToString(context)
 	if err != nil {
 		return "", fmt.Errorf("error rendering template: %w", err)
 	}
 
 	return message, nil
+}
+
+func getGonjaTemplate(source string) (*exec.Template, error) {
+	// Get custom filters
+	customFilters := getGonjaFilters()
+
+	// Create a new filter set with custom filters
+	customFilterSet := exec.NewFilterSet(customFilters)
+
+	// Merge with default filters using Update method
+	filterSet := gonja.DefaultEnvironment.Filters.Update(customFilterSet)
+
+	// Create environment with merged filters and methods
+	env := &exec.Environment{
+		Context:           gonja.DefaultEnvironment.Context,
+		Filters:           filterSet,
+		ControlStructures: gonja.DefaultEnvironment.ControlStructures,
+		Tests:             gonja.DefaultEnvironment.Tests,
+		Methods:           gonja.DefaultEnvironment.Methods,
+	}
+
+	sourceBytes := []byte(source)
+	rootID := fmt.Sprintf("root-%s", string(sha256.New().Sum(sourceBytes)))
+
+	loader, err := loaders.NewFileSystemLoader("")
+	if err != nil {
+		return nil, err
+	}
+	shiftedLoader, err := loaders.NewShiftedLoader(rootID, bytes.NewReader(sourceBytes), loader)
+	if err != nil {
+		return nil, err
+	}
+
+	return exec.NewTemplate(rootID, gonja.DefaultConfig, shiftedLoader, env)
 }
 
 func CompareStatementMessage(message1, message2 string) (bool, string) {
